0f8088f19c211602a595b7c468e9ac627157ca09
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2025-12-02 22:44:21 +0300

    first commit

---
+++ lib/stack.py
@@ -0,0 +1,35 @@
+class Stack:
+
+    def __init__(self, items=None, limit=100):
+        self.items = items[:] if items else []
+        self.limit = limit
+
+    def isEmpty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        if self.full():
+            return
+        self.items.append(item)
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        return self.items.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.items[-1]
+    
+    def size(self):
+        return len(self.items)
+
+    def full(self):
+        return len(self.items) >= self.limit
+
+    def search(self, target):
+        try:
+            return len(self.items) - 1 - self.items.index(target)
+        except ValueError:
+            return -1
--- lib/testing/stack_test.py
+++ lib/testing/stack_test.py
@@ -1,4 +1,4 @@
-from Stack import Stack
+from stack import Stack
 
 class TestStack:
     '''Class Stack in Stack.py'''
